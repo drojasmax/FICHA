3f2fbb8d3444001a0bf57cb6a0db0bef9d7d15a7
DanDie <jonatanfuentes236@gmail.com>
2022-07-26 19:02:32 -0400

    redireccionar_pasajero_conductor.java, crear_conductor.java Listos

---
+++ app/src/main/java/com/example/ejemplo/redireccionar_pasajero_conductor.java
@@ -0,0 +1,30 @@
+package com.example.ejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class redireccionar_pasajero_conductor extends AppCompatActivity {
+    private EditText etPhone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redireccionar_pasajero_conductor);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+
+    }
+    //Metodo para boton Enviar
+    public void Enviar(View view){
+        Intent i = new Intent(this, crear_conductor.class);
+        i.putExtra("dato", etPhone.getText().toString());
+        startActivity(i);
+
+    }
+
+}
+
+
